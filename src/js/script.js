00e7b195a8098f88e8c3016b4b9d6c0d807a3dcf
Ризобек Бабажанов <nazimaibragimova7582@gmail.com>
2025-10-09 06:06:40 +0500

    Media

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+    window.addEventListener('DOMContentLoaded', () => {
+        const list = document.querySelector('.list'),
+        listItem = document.querySelectorAll('.list_item'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          list.classList.toggle('list_active');
+        });
+    
+        listItem.forEach(item => {
+          item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            list.classList.toggle('list_active');
+          })
+        })
+      });
+
+    $('ul.menu_tabs').on('click', 'li:not(.menu_tab_active)', function() {
+        $(this)
+          .addClass('menu_tab_active').siblings().removeClass('menu_tab_active')
+    });
+
     new WOW().init();
 });
